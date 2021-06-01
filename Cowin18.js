080e3b16877224058530428250628f02891bacd4
sanjaykm86 <sanjaykmurali@gmail.com>
2021-06-01 12:40:48 +0530

    added node-powershell

--- Cowin18.js
+++ Cowin18.js
@@ -1,12 +1,28 @@
-const childProcess = require('child_process').spawn
-const powershell = childProcess('powershell.exe',['.\\Cowin.ps1'])
-powershell.stdout.on("data",function(data){
-    console.log("Powershell data : "+data)
-})
-powershell.stderr.on("data",function(data){
-    console.log("Powershell Errors: " + data);
+// const childProcess = require('child_process').spawn
+// const powershell = childProcess('powershell.exe',['.\\Cowin.ps1'])
+// powershell.stdout.on("data",function(data){
+//     console.log("Powershell data : "+data)
+// })
+// powershell.stderr.on("data",function(data){
+//     console.log("Powershell Errors: " + data);
+// });
+// powershell.on("exit",function(){
+//     console.log("Powershell Script finished");
+// });
+// powershell.stdin.end(); //end input
+
+const shell = require('node-powershell');
+let ps = new shell({
+  executionPolicy: 'Bypass',
+  noProfile: true
 });
-powershell.on("exit",function(){
-    console.log("Powershell Script finished");
+
+ps.addCommand('echo node-powershell')
+ps.addCommand('./Cowin.ps1')
+
+ps.invoke().then(output => {
+  console.log(output);
+}).catch(err => {
+  console.log(err);
+  ps.dispose();
 });
-powershell.stdin.end(); //end input
