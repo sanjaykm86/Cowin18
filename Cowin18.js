de8c4b989a2340ea9830309303f431b0c76b2303
sanjaykm86 <sanjaykmurali@gmail.com>
2021-06-02 11:26:08 +0530

    added powershell core..testing

--- Cowin18.js
+++ Cowin18.js
@@ -12,9 +12,12 @@
 // powershell.stdin.end(); //end input
 
 const shell = require('node-powershell');
+// let ps = new shell('./Cowin18.ps1')
+
 let ps = new shell({
   executionPolicy: 'Bypass',
-  noProfile: true
+  noProfile: true,
+  usePwsh:true
 });
 
 ps.addCommand('echo node-powershell')
@@ -26,3 +29,16 @@ ps.invoke().then(output => {
   console.log(err);
   ps.dispose();
 });
+// ps.on("error", err => {
+//   console.error(err);
+// });
+
+// // Stdout
+// ps.on("output", data => {
+//   console.log(data);
+// });
+
+// // Stderr
+// ps.on("error-output", data => {
+//   console.error(data);
+// });
